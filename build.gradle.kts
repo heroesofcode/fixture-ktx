a12d1004b238ad35399e4c6a3bee5b7e657f3030
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2024-05-29 23:41:32 -0300

    Adding github actions for CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+
     id("io.gitlab.arturbosch.detekt") version "1.23.2"
+    id("org.jetbrains.kotlinx.kover") version "0.7.6"
 }
 
 group = "org.example"
