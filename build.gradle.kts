80970a5929dc3f3a1c72df8d5f813af736a4d7be
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2024-05-30 00:27:18 -0300

    Automate publish to Github Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,24 @@ plugins {
 
     id("io.gitlab.arturbosch.detekt") version "1.23.2"
     id("org.jetbrains.kotlinx.kover") version "0.7.6"
+
+    `maven-publish`
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.heroesofcode"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
+
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/heroesofcode/fixture-ktx")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+            password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 dependencies {
@@ -24,4 +35,25 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = "com.heroesofcode"
+            artifactId = "fixture-ktx"
+            version = "0.1.0"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/heroesofcode/fixture-ktx")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GIT_ACTOR")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GIT_TOKEN")
+            }
+        }
+    }
 }
